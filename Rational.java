2dadfd8acc01648edaba96d8bc6640b726e18303
Zabari <zabari.daniel@Gmail.com>
2013-11-18 23:52:02 -0500

    Phase II

--- Rational.java
+++ Rational.java
@@ -33,7 +33,33 @@ public class Rational{
 	this._numer*=num._denomer;
 	this._denomer*=num._numer;
     }
-
+    public void add(Rational num){
+	this._numer=this._numer*num._denomer+num._numer*this._denomer;
+	this._denomer*=num._denomer;
+    }
+    public void subtract(Rational num){
+	this._numer=this._numer*num._denomer-num._numer*this._denomer;
+	this._denomer*=num._denomer;
+    }
+    public static int gcdew(int a, int b){
+	int x=a;
+	a=Math.max(a,b);
+	b=Math.min(x,b);
+	while (b>0){
+	    x=a-b;
+	    a=Math.max(x,b);
+	    b=Math.min(x,b);
+	}
+	return a;
+    }
+    public int gcd(){
+	return gcdew(this._numer,this._denomer);
+    }
+    public void reduce(){
+	int x=gcd();
+	this._numer/=x;
+	this._denomer/=x;
+    }
 
     public static void main(String[] args){
 	Rational numy=new Rational(4,6);
@@ -43,6 +69,13 @@ public class Rational{
 	numy.multiply(num2);
 	System.out.println(numy);
 	numy.divide(num2);
+	System.out.println(numy);
+	numy.add(num2);
+	System.out.println(numy);
+	numy.subtract(num2);
+	System.out.println(numy);
+	System.out.println(numy.gcd());
+	numy.reduce();
 	System.out.println(numy);
 	
 
